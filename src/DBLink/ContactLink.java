bf5afea188b45263046acf4f464881ee2de1549b
Scott Daily <scottjuggles@gmail.com>
2021-03-19 12:59:30 -0400

    Added ContactLink.java

--- src/DBLink/ContactLink.java
+++ src/DBLink/ContactLink.java
@@ -1,4 +1,39 @@
 package DBLink;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import models.Appointment;
+import models.Contact;
+import utils.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ContactLink {
+
+    public static ObservableList<Contact> getAllContacts() {
+
+        ObservableList<Contact> contactList = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * from contacts";
+
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int contactID = rs.getInt("Contact_ID");
+                String name = rs.getString("Contact_Name");
+                String email = rs.getString("Email");
+
+                Contact contact = new Contact(contactID, name, email);
+                contactList.add(contact);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return contactList;
+    }
 }
